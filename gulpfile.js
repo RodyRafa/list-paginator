57f8b3559b6f1569a1f7e4c68fedbd7f0408fbe8
RodyRafa <me@rodyrafa.com.br>
2015-08-21 00:40:45 -0300

    [WIP] Added the jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-var gulp = require('gulp');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var gulp = require('gulp');
 
 gulp.task('default', function () {
   return gulp.src(['src/umd/head.js', './src/list-paginator.js', './src/umd/foot.js'])
           .pipe(concat('list-paginator.js'))
           .pipe(gulp.dest('./dist/'));  
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('./src/*.js')
+          .pipe(jshint())
+          .pipe(jshint.reporter('default'));
 });
